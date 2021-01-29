ea99feea6cd144205d837b187285bfa29b0dc976
Thi Nguyen <thiman@gmail.com>
2021-01-29 21:50:02 +0000

    organization

--- script.js
+++ script.js
@@ -53,14 +53,3 @@ $('#reset-btn').on('click', function() {
   
   $('#weights-list li.removable').remove();
 });
-
-// handle caching
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/serviceWorker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
-}
