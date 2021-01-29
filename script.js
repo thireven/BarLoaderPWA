8ca02a2ddf3ea38ad5de3d3469287130f9f558e6
Thi Nguyen <thiman@gmail.com>
2021-01-29 01:43:29 +0000

    v1 done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function kilogramToPounds(kgUnit) {
+  return Number.parseFloat(kgUnit) * 2.20462262185;
+}
+
+function updateWeightInfoDisplay() {
+  $(display.kg).html(currentWeight.toFixed(2) + ' kg');
+  $(display.lbs).html(kilogramToPounds(currentWeight).toFixed(1) + ' lbs');
+}
+
 const weightMap = {
   50: { color: '#188034' },
   25: { color: '#c42121' },
@@ -9,6 +18,14 @@ const weightMap = {
   1.25: { color: '#c4c4c4' },
 }
 
+const display = {
+  kg: $('#info-weight-kg'),
+  lbs: $('#info-weight-lbs'),
+} 
+
+let currentWeight = 20;
+
+const weightList = $('#weights-list');
 const weightButtons = $('.weight-btn');
 
 weightButtons.each(btn => {
@@ -18,4 +35,21 @@ weightButtons.each(btn => {
   
   button.css('background', weightData.color);
   button.html(weight + 'kg');
-})
+});
+
+weightButtons.on('click', e => {
+  const weight = $(e.target).data('weight');
+  
+  $(weightList).append(`<li class="removable"><div class="weight-disc" data-weight="${weight}"></div></li>`);
+  
+  currentWeight += parseFloat(weight) * 2;
+  
+  updateWeightInfoDisplay();
+});
+
+$('#reset-btn').on('click', function() {
+  currentWeight = 20;
+  updateWeightInfoDisplay();
+  
+  $('#weights-list li.removable').remove();
+});
