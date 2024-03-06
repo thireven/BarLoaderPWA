7e07d80fe8cc9e8254511a61d119e4f5b21e7f6e
Thi Nguyen <thiman@gmail.com>
2024-03-05 21:46:04 -0800

    cache buster

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,10 +5,11 @@ self.addEventListener('install', function (event) {
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(
         [
-          '/style.css',
-          '/script.js',
+          '/style.css?v=20240305',
+          '/script.js?v=20240305',
           '/vendor/cash.min.js',
-          '/index.html'
+          '/vendor/draggable.min.js',
+          '/index.html?v=20240305'
         ]
       );
     })
